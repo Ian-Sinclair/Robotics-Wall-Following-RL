0d6f1847f5efd04eef9468b15478daa7a202a487
Ian Sinclair <ian.sinclair@du.edu>
2023-03-17 20:44:10 -0600

    Wall Following Navigation Software

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iansinclair/github/COMP-4510-Project-3/catkin_ws/src
-# Build directory: /home/iansinclair/github/COMP-4510-Project-3/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sair_course_project3")
-subdirs("wallfollowing")
--- catkin_ws/build/wallfollowing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iansinclair/github/COMP-4510-Project-3/catkin_ws/src/wallfollowing
-# Build directory: /home/iansinclair/github/COMP-4510-Project-3/catkin_ws/build/wallfollowing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
